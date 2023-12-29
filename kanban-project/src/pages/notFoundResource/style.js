828ad2cf0d915574ca498748d913d9fd4180b708
Lanakosuhina <lana.kosuhina@gmail.com>
2023-12-30 00:53:59 +0300

    changed page 404

--- kanban-project/src/pages/notFoundResource/style.js
+++ kanban-project/src/pages/notFoundResource/style.js
@@ -15,7 +15,7 @@ export const fadeintext = keyframes`
     color: #ffa100;
     text-shadow: 0px 0px 4px #ffa100;
   }
-`
+`;
 
 export const flicker4 = keyframes`
   0% {
@@ -82,7 +82,7 @@ export const flicker4 = keyframes`
     color: #ffa100;
     text-shadow: 0px 0px 4px #ffa100;
   }
-`
+`;
 
 export const flicker1 = keyframes`
   0% {
@@ -120,7 +120,7 @@ export const flicker1 = keyframes`
   100% {
     stroke: #ffa100;
   }
-`
+`;
 
 export const flicker2 = keyframes`
   0% {
@@ -146,7 +146,7 @@ export const flicker2 = keyframes`
   100% {
     stroke: #ffa100;
   }
-`
+`;
 
 export const flicker3 = keyframes`
   0% {
@@ -184,7 +184,7 @@ export const flicker3 = keyframes`
   100% {
     stroke: #ffa100;
   }
-`
+`;
 
 export const drawline1 = keyframes`
   0% {
@@ -194,7 +194,7 @@ export const drawline1 = keyframes`
   100% {
     stroke-dashoffset: 0px;
   }
-`
+`;
 
 export const drawline2 = keyframes`
   0% {
@@ -204,7 +204,7 @@ export const drawline2 = keyframes`
   100% {
     stroke-dashoffset: 0px;
   }
-`
+`;
 
 export const drawline3 = keyframes`
   0% {
@@ -214,7 +214,7 @@ export const drawline3 = keyframes`
   100% {
     stroke-dashoffset: 0px;
   }
-`
+`;
 
 export const huerotate = keyframes`
   0% {
@@ -228,27 +228,14 @@ export const huerotate = keyframes`
   100% {
     filter: hue-rotate(0deg);
   }
-`
+`;
 
 export const GlobalStyleNotFound = createGlobalStyle`
 
 @import url('https://fonts.googleapis.com/css?family=Audiowide&display=swap');
+`;
 
-/* html,
-body {
-  margin: 0px;
-  overflow: hidden;
-} */
-
-.wrap {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
-
-#svgWrap_1,
-#svgWrap_2 {
+export const SvgWrap_1 = styled.svg`
   position: absolute;
   height: auto;
   width: 600px;
@@ -256,52 +243,76 @@ body {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-}
-
-#svgWrap_1,
-#svgWrap_2,
-div {
   animation: ${huerotate} 6s ease-in-out 3s infinite;
-}
+`;
+export const SvgWrap_2 = styled.svg`
+  position: absolute;
+  height: auto;
+  width: 600px;
+  max-width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  animation: ${huerotate} 6s ease-in-out 3s infinite;
+`;
 
-#id1_1,
-#id2_1,
-#id3_1 {
-  stroke: #FFA100;
+export const NotFoundPageBody = styled.body`
+  margin: 0px;
+  overflow: hidden;
+`;
+
+export const Id1_1 = styled.path`
+  stroke: #fccd7c;
   stroke-width: 3px;
   fill: transparent;
   filter: url(#glow);
-}
-
-#id1_2,
-#id2_2,
-#id3_2 {
-  stroke: #565eef;
-  stroke-width: 3px;
-  fill: transparent;
-  filter: url(#glow);
-}
-
-#id3_1 {
   stroke-dasharray: 940px;
   stroke-dashoffset: -940px;
-  animation: ${drawline3} 2.5s ease-in-out 0s forwards, ${flicker3} 4s linear 4s infinite;
-}
+  animation: ${drawline1} 2.5s ease-in-out 1s forwards,
+    ${flicker1} 4s linear 5s infinite;
+`;
 
-#id2_1 {
+export const Id2_1 = styled.path`
+  stroke: #fccd7c;
+  stroke-width: 3px;
+  fill: transparent;
+  filter: url(#glow);
   stroke-dasharray: 735px;
   stroke-dashoffset: -735px;
-  animation: ${drawline2} 2.5s ease-in-out 0.5s forwards, ${flicker2} 4s linear 4.5s infinite;
-}
+  animation: ${drawline2} 2.5s ease-in-out 0.5s forwards,
+    ${flicker2} 4s linear 4.5s infinite;
+`;
 
-#id1_1 {
+export const Id3_1 = styled.path`
+  stroke: #fccd7c;
+  stroke-width: 3px;
+  fill: transparent;
+  filter: url(#glow);
   stroke-dasharray: 940px;
   stroke-dashoffset: -940px;
-  animation: ${drawline1} 2.5s ease-in-out 1s forwards, ${flicker1} 4s linear 5s infinite;
-}
+  animation: ${drawline3} 2.5s ease-in-out 0s forwards,
+    ${flicker3} 4s linear 4s infinite;
+`;
+export const Id1_2 = styled.path`
+  stroke: #a0a4fd;
+  stroke-width: 3px;
+  fill: transparent;
+  filter: url(#glow);
+`;
+export const Id2_2 = styled.path`
+  stroke: #a0a4fd;
+  stroke-width: 3px;
+  fill: transparent;
+  filter: url(#glow);
+`;
+export const Id3_2 = styled.path`
+  stroke: #a0a4fd;
+  stroke-width: 3px;
+  fill: transparent;
+  filter: url(#glow);
+`;
 
-`
-export const PageDiv = styled.div`
+export const NotFoundPageDiv = styled.div`
   position: absolute;
   top: 0%;
   left: 0%;
@@ -310,8 +321,10 @@ export const PageDiv = styled.div`
   margin: 0px;
   background: radial-gradient(circle, #fccd7c 0%, #a0a4fd 100%);
   overflow: hidden;
-`
-export const Text = styled.h2`
+  animation: ${huerotate} 6s ease-in-out 3s infinite;
+`;
+
+export const NotFoundPageH2 = styled.h2`
   position: absolute;
   top: 50%;
   left: 50%;
@@ -324,5 +337,6 @@ export const Text = styled.h2`
   font-weight: 300;
   font-family: Audiowide;
   text-shadow: 0px 0px 4px #565eef;
-  animation: ${fadeintext} 3s ease-in 3.5s forwards, flicker4 5s linear 7.5s infinite, hueRotate 6s ease-in-out 3s infinite;
-`
+  animation: ${fadeintext} 3s ease-in 3.5s forwards,
+    ${flicker4} 5s linear 7.5s infinite, ${huerotate} 6s ease-in-out 3s infinite;
+`;
